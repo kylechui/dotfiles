bcaf81b66bdb946f4cf808488984c473c61fd833
Kyle Chui <kyle.chui+github@pm.me>
2023-11-13 22:24:08 -0800

    feat: Use `<C-Space>` for autosuggestions.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -5,10 +5,14 @@
       e = "nvim";
       ll = "ls -al";
       rm = "rm -I";
+      gst = "git status";
       nf = "nix flake";
       nfi = "nix flake init";
       nfu = "nix flake update";
       nd = "nix develop";
+    };
+    functions = {
+      fish_user_key_bindings = { body = "bind -k nul accept-autosuggestion"; };
     };
   };
 }
