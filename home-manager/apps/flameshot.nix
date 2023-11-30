3135a52120361228882c60949828cde5dadf990d
Kyle Chui <kyle.chui+github@pm.me>
2023-11-29 23:25:27 -0800

    fix: Remove `flameshot` buttons.

--- home-manager/apps/flameshot.nix
+++ home-manager/apps/flameshot.nix
@@ -8,7 +8,8 @@
         showHelp = false;
         showStartupLaunchMessage = false;
         # Use `flameshot config` to choose the buttons first, and export to a file
-        buttons = "@Variant(\0\0\0\x7f\0\0\0\vQList<int>\0\0\0\0\t\0\0\0\0\0\0\0\x1\0\0\0\x2\0\0\0\x3\0\0\0\x4\0\0\0\x5\0\0\0\x12\0\0\0\xf\0\0\0\a)";
+        buttons =
+          "@Variant(\\0\\0\\0\\x7f\\0\\0\\0\\vQList<int>\\0\\0\\0\\0\\t\\0\\0\\0\\0\\0\\0\\0\\x1\\0\\0\\0\\x2\\0\\0\\0\\x3\\0\\0\\0\\x4\\0\\0\\0\\x5\\0\\0\\0\\x12\\0\\0\\0\\xf\\0\\0\\0\\a)";
       };
     };
   };
