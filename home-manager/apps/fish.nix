61ae4cf91dd5f8415ecc3b0e2d3a6bec77909570
Kyle Chui <kyle.chui+github@pm.me>
2023-12-10 18:31:49 -0800

    refactor(fish): Clean up configuration.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -20,33 +20,18 @@
     };
     shellInit = ''
       abbr --add !! --position anywhere --function last_history_item
+      source ${
+        builtins.fetchurl {
+          url =
+            "https://raw.githubusercontent.com/rebelot/kanagawa.nvim/c19b9023842697ec92caf72cd3599f7dd7be4456/extras/kanagawa.fish";
+          sha256 =
+            "sha256:0smmy783j41294gda1mpq8bqdy7h7j69zhh2i0dgxdg4gxqm7i6s";
+        }
+      }
     '';
-    plugins = [
-      {
-        name = "done";
-        src = pkgs.fetchFromGitHub {
-          owner = "franciscolourenco";
-          repo = "done";
-          rev = "fbea3f682f9f32d957946490436e9dde8a67c367";
-          sha256 = "sha256-BGHfwKoMfOZUsa05kEt8W2luc1aC3Su/OyaGmcb4UiI=";
-        };
-      }
-      {
-        name = "kanagawa";
-        src = pkgs.stdenv.mkDerivation {
-          name = "kanagawa-fish";
-          src = pkgs.fetchFromGitHub {
-            owner = "rebelot";
-            repo = "kanagawa.nvim";
-            rev = "c19b9023842697ec92caf72cd3599f7dd7be4456";
-            sha256 = "sha256-pbLcomZHzC2JGKF4oII6AAm5q/dzQtNfFAZVNX74nB8=";
-          };
-          buildPhase = ''
-            mkdir -p $out
-            mv extras/kanagawa.fish $out/init.fish
-          '';
-        };
-      }
-    ];
+    plugins = [{
+      name = "done";
+      src = pkgs.fishPlugins.done;
+    }];
   };
 }
