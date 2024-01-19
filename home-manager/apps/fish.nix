90f23c9e9750cab5b27ae586d84e3183ccbe2f24
Kyle Chui <kyle.chui+github@pm.me>
2024-01-19 15:50:37 -0800

    fix(fish): Force `vi` cursor.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -5,6 +5,8 @@
     enable = true;
     interactiveShellInit = ''
       set -g fish_key_bindings fish_vi_key_bindings 
+      set -g fish_vi_force_cursor 1
+      set -g fish_cursor_default block
       set -g fish_cursor_default block
       set -g fish_cursor_visual block
       set -g fish_cursor_insert line
