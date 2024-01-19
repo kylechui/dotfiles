565ddf4afaedc93df3abd72b6835f7c4891a9536
Kyle Chui <kyle.chui+github@pm.me>
2024-01-19 15:47:18 -0800

    feat(fish): Set `fish` cursor shape based on `vi` mode.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -5,6 +5,11 @@
     enable = true;
     interactiveShellInit = ''
       set -g fish_key_bindings fish_vi_key_bindings 
+      set -g fish_cursor_default block
+      set -g fish_cursor_visual block
+      set -g fish_cursor_insert line
+      set -g fish_cursor_replace underscore
+      set -g fish_cursor_replace_one underscore
     '';
     shellAliases = {
       e = "nvim";
