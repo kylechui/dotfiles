4ca5f7b40cc06a32da216321d911bcc393164b47
Kyle Chui <kyle.chui+github@pm.me>
2024-01-20 14:35:57 -0800

    feat(ncspot): Add some keymaps and other configuration.

--- home-manager/apps/ncspot.nix
+++ home-manager/apps/ncspot.nix
@@ -6,8 +6,22 @@
     package = pkgs.unstable.ncspot;
     settings = {
       backend = "pulseaudio";
-      use_nerdfont = true;
       flip_status_indicators = true;
+      library_tabs = [ "tracks" "playlists" ];
+      repeat = "playlist";
+      use_nerdfont = true;
+      keybindings = {
+        "a" = "focus library";
+        "d" = "noop";
+        "s" = "shuffle";
+        "q" = "focus queue";
+        "y" = "share selected";
+        "Space" = "toggleplayback";
+        "Ctrl+f" = "search";
+        "Shift+j" = "move down 5";
+        "Shift+k" = "move up 5";
+        "Shift+q" = "quit";
+      };
     };
   };
 }
