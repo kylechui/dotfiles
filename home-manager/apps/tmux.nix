dea837daffae42b3f48464dd9c470d5c989cbc07
Kyle Chui <kyle.chui+github@pm.me>
2024-02-02 19:35:51 -0800

    feat(tmux): Proper term colors, remove `<Esc>` lag.

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -3,8 +3,14 @@
 {
   programs.tmux = {
     enable = true;
+    terminal = "screen-256color";
     extraConfig = ''
+      # Set the default prompt to fish, without overriding the default shell
       set -g default-command "${pkgs.fish}/bin/fish -l -i"
+      # Enable tmux true color support
+      set -sa terminal-features ",xterm-256color:RGB"
+      # Avoid laggy escape key in nvim
+      set -sg escape-time 0
     '';
   };
 }
