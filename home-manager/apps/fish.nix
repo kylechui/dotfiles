8896b9b8561fb868696dbad8e0351cdf0941164f
Kyle Chui <kyle.chui+github@pm.me>
2024-02-20 12:33:46 -0800

    refactor(fish): Use `shellAbbrs` instead of `shellAliases`.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -16,21 +16,22 @@
       set -g man_standout -b 938aa9 1f1f28
     '';
     shellAliases = {
-      e = "nvim";
       ll = "ls -al";
       rm = "rm -I";
-      gm = "git mergetool";
       gc = "git_checkout";
       gwa = "git_worktree_add";
       gwr = "git_worktree_remove";
-      gf = "git fetch --all";
-      gst = "git status";
+      copy = "${pkgs.coreutils}/bin/tee (${pkgs.coreutils}/bin/tty) | ${pkgs.xclip}/bin/xclip -selection clipboard";
+    };
+    shellAbbrs = {
+      e = "nvim";
+      cat = "bat";
+      nd = "nix develop";
       nf = "nix flake";
-      nfi = "nix flake init";
-      nfu = "nix flake update";
-      nd = "nix develop --max-jobs auto --builders 'cores = 0'";
-      cat = "${pkgs.bat}/bin/bat";
-      copy = "tee (tty) | xclip -selection clipboard";
+      gl = "git log";
+      gm = "git mergetool";
+      gf = "git fetch";
+      gst = "git status";
     };
     functions = {
       find_git_repository = {
@@ -128,9 +129,14 @@
           bind -M insert -k backspace backward-kill-word # This is <C-BS>
         '';
       };
-      fish_greeting = { body = ""; };
+      fish_greeting = {
+        body = "";
+      };
       pretty_ms = {
-        argumentNames = [ "ms" "interval" ];
+        argumentNames = [
+          "ms"
+          "interval"
+        ];
         body = ''
           set -l interval_ms
           set -l scale 1
@@ -208,16 +214,16 @@
           set_color normal
         '';
       };
-      last_history_item = { body = "echo -- $history[1]"; };
+      last_history_item = {
+        body = "echo -- $history[1]";
+      };
     };
     shellInit = ''
       abbr --add !! --position anywhere --function last_history_item
       source ${
         builtins.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/rebelot/kanagawa.nvim/c19b9023842697ec92caf72cd3599f7dd7be4456/extras/kanagawa.fish";
-          sha256 =
-            "sha256:0smmy783j41294gda1mpq8bqdy7h7j69zhh2i0dgxdg4gxqm7i6s";
+          url = "https://raw.githubusercontent.com/rebelot/kanagawa.nvim/c19b9023842697ec92caf72cd3599f7dd7be4456/extras/kanagawa.fish";
+          sha256 = "sha256:0smmy783j41294gda1mpq8bqdy7h7j69zhh2i0dgxdg4gxqm7i6s";
         }
       }
     '';
