aef52a3dee0288ccccb22a839c0be80e79e06444
Kyle Chui <kyle.chui+github@pm.me>
2024-02-21 14:49:30 -0800

    fix(fish): Cursor shape.

--- home-manager/apps/fish.nix
+++ home-manager/apps/fish.nix
@@ -7,11 +7,12 @@
       set -g fish_key_bindings fish_vi_key_bindings 
       set -g fish_vi_force_cursor 1
       set -g fish_cursor_default block
-      set -g fish_cursor_default block
       set -g fish_cursor_visual block
       set -g fish_cursor_insert line
       set -g fish_cursor_replace underscore
       set -g fish_cursor_replace_one underscore
+      # Set the cursor shape in external shells/REPLs
+      set -g fish_cursor_external line
       # Set manpage search color
       set -g man_standout -b 938aa9 1f1f28
     '';
