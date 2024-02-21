9233946bfef15466ade42c8086327501a81faa50
Kyle Chui <kyle.chui+github@pm.me>
2024-02-20 19:12:09 -0800

    feat: Pin home-manager version.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
