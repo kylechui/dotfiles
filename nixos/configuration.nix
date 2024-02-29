092c2ae6f828a09913bc3e05d62a20d0c8349536
Kyle Chui <kyle.chui+github@pm.me>
2024-02-28 16:01:33 -0800

    feat(tlp): Add basic configuration.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,7 +54,10 @@
 
   # Enable flakes by default
   nix.package = pkgs.nix;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -109,7 +112,21 @@
   };
 
   # Power management
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+    };
+  };
   services.thermald.enable = true;
 
   # Bluetooth management
@@ -119,7 +136,10 @@
   users.users.kylec = {
     isNormalUser = true;
     description = "Kyle Chui";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     shell = pkgs.bash;
     packages = [ ];
   };
