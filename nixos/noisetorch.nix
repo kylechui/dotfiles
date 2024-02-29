89a5321ee51290d12530513bbe25db325405a908
Kyle Chui <kyle.chui+github@pm.me>
2024-02-28 22:02:43 -0800

    fix(noisetorch): Systemd script `ExecStart`.

--- nixos/noisetorch.nix
+++ nixos/noisetorch.nix
@@ -5,8 +5,9 @@
   # https://github.com/noisetorch/NoiseTorch/wiki/Start-automatically-with-Systemd
   systemd.user.services.noisetorch = {
     description = "Noisetorch Noise Cancelling";
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "default.target" ];
     after = [
+      "pipewire.service"
       "pipewire-pulse.service"
       "sys-devices-pci0000:00-0000:00:1f.3-skl_hda_dsp_generic-sound-card0-controlC0.device"
     ];
@@ -16,7 +17,7 @@
     serviceConfig = {
       Type = "simple";
       RemainAfterExit = true;
-      ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i -t 85";
+      ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i -s alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source -t 85";
       ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
       Restart = "on-failure";
       RestartSec = 3;
