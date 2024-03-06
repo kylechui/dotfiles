f78a423c66008aa96bbc0f1027beabdbcbc1b61b
Kyle Chui <kyle.chui+github@pm.me>
2024-03-05 20:35:53 -0800

    refactor: Use Jishnu's `systemd-sleep-hook`.

--- nixos/suspend.nix
+++ nixos/suspend.nix
@@ -1,17 +1,34 @@
 { pkgs, ... }:
 
+let
+  pname = "systemd-sleep-hook";
+  systemd-sleep-hook = pkgs.python3Packages.buildPythonPackage {
+    inherit pname;
+    version = "0.0.1";
+    format = "pyproject";
+    nativeBuildInputs = [ pkgs.python311Packages.setuptools ];
+    propagatedBuildInputs = [
+      pkgs.python311Packages.dbus-python
+      pkgs.python311Packages.pygobject3
+    ];
+    src = pkgs.fetchFromGitHub {
+      owner = "jishnusen";
+      repo = pname;
+      rev = "da6190a9103970f5b6874890662c9f82934e5c8c";
+      hash = "sha256-0DnyFUBeXElsT2ndjDPM5J4yG0ngUeAOyqt1o5UGugE=";
+    };
+  };
+in
 {
-  # This is considered a "hack" that uses systemd-sleep[1]; I should figure out
-  # how to use inhibitor pattern[2] to make this work.
-  # [1]: https://www.freedesktop.org/software/systemd/man/latest/systemd-sleep.html
-  # [2]: https://systemd.io/INHIBITOR_LOCKS/
-  environment.etc."systemd/system-sleep/suspend-toggle-radios.sh".source = pkgs.writeShellScript "suspend-toggle-radios.sh" ''
-    if [ "$1" = "pre" ]; then
-      ${pkgs.util-linux}/bin/rfkill block bluetooth
-      ${pkgs.util-linux}/bin/rfkill block wifi
-    elif [ "$1" = "post" ]; then
-      ${pkgs.util-linux}/bin/rfkill unblock bluetooth
-      ${pkgs.util-linux}/bin/rfkill unblock wifi
-    fi
-  '';
+  environment.systemPackages = [ systemd-sleep-hook ];
+  systemd.user.services."systemd-sleep-hook" = {
+    description = "Toggle radios on suspend/resume";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${systemd-sleep-hook}/bin/systemd-sleep-hook -s '${pkgs.util-linux}/bin/rfkill block all' -r '${pkgs.util-linux}/bin/rfkill unblock all'
+      '';
+    };
+  };
 }
