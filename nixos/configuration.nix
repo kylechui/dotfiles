ce200a3e97f4515272ba7299e042507dc1cf97c7
Kyle Chui <kyle.chui+github@pm.me>
2024-03-20 13:32:18 -0700

    fix: Remove dead code.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -161,8 +161,6 @@
     memoryPercent = 50;
   };
 
-  services.gvfs.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
