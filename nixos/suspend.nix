2286d7340220e6fa143f4d15ff888a1a656fe94e
Kyle Chui <kyle.chui+github@pm.me>
2024-03-20 13:52:51 -0700

    fix: `systemd-sleep-hook` incorrect `wantedBy` target.

--- nixos/suspend.nix
+++ nixos/suspend.nix
@@ -4,7 +4,7 @@ let
   pname = "systemd-sleep-hook";
   systemd-sleep-hook = pkgs.python3Packages.buildPythonPackage {
     inherit pname;
-    version = "0.0.1";
+    version = "0.0.3";
     format = "pyproject";
     nativeBuildInputs = [ pkgs.python311Packages.setuptools ];
     propagatedBuildInputs = [
@@ -14,8 +14,8 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "jishnusen";
       repo = pname;
-      rev = "da6190a9103970f5b6874890662c9f82934e5c8c";
-      hash = "sha256-0DnyFUBeXElsT2ndjDPM5J4yG0ngUeAOyqt1o5UGugE=";
+      rev = "732c20624b6387a8758091105a425f9d019c3045";
+      hash = "sha256-QJF9Ud/dizCdo9Y6nZXWQiVHpVuzRdxCLxv+UniAmRo=";
     };
   };
 in
@@ -23,13 +23,14 @@ in
   environment.systemPackages = [ systemd-sleep-hook ];
   systemd.user.services."systemd-sleep-hook" = {
     description = "Toggle radios on suspend/resume";
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = ''
         ${systemd-sleep-hook}/bin/systemd-sleep-hook -s '${pkgs.util-linux}/bin/rfkill block all' -r '${pkgs.util-linux}/bin/rfkill unblock all'
       '';
       Restart = "always";
+      RestartSec = 1;
     };
   };
   systemd.user.services."xss-lock" = {
