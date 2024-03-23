d1a9e0a04fa4853ac874d6b86b5dfdebc694449e
Kyle Chui <kyle.chui+github@pm.me>
2024-03-22 18:58:16 -0700

    feat: Patch `Iosevka Term`.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -61,96 +61,110 @@
     extraPackages = tpkgs: { inherit (tpkgs) scheme-full; };
   };
 
-  home.packages = with pkgs; [
-    # CLI Utilities
-    zip
-    unzip
-    xclip
-    ripgrep
-    arandr
-    tree
-    pandoc
-    libnotify
-    sshfs
-    fd
-    jq
-    yq
-    glxinfo
-    pciutils
-    sysstat
-    nixpkgs-review
-    pkg-config
-    fontconfig
-    # Social
-    element-desktop
-    signal-desktop
-    discord
-    zoom-us
-    # Other apps
-    spotify
-    pavucontrol
-    obsidian
-    libreoffice-qt
-    screenkey
-    simplescreenrecorder
-    xfce.thunar
+  home.packages =
+    with pkgs;
+    let
+      iosevka-term = (
+        iosevka.override {
+          set = "Term";
+          privateBuildPlan = {
+            family = "Iosevka Term";
+          };
+        }
+      );
+    in
+    [
+      # CLI Utilities
+      zip
+      unzip
+      xclip
+      ripgrep
+      arandr
+      tree
+      pandoc
+      libnotify
+      sshfs
+      fd
+      jq
+      yq
+      glxinfo
+      pciutils
+      sysstat
+      nixpkgs-review
+      pkg-config
+      fontconfig
+      # Social
+      element-desktop
+      signal-desktop
+      discord
+      zoom-us
+      # Other apps
+      spotify
+      pavucontrol
+      obsidian
+      libreoffice-qt
+      screenkey
+      simplescreenrecorder
+      xfce.thunar
 
-    # Fonts
-    iosevka
-    sarasa-gothic
-    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-    # Shell
-    shellcheck
-    # C/C++
-    cmake
-    gnumake
-    gcc
-    clang-tools
-    # Python
-    python311
-    black
-    mypy
-    nodePackages.pyright
-    # JavaScript/TypeScript
-    nodejs
-    typescript
-    nodePackages.typescript-language-server
-    unstable.prettierd
-    # OCaml
-    ocamlformat
-    ocamlPackages.ocaml-lsp
-    dune_3
-    # Nix
-    unstable.nil
-    nixfmt
-    # Lua
-    sumneko-lua-language-server
-    stylua
-    selene
-    # LaTeX
-    python311Packages.pygments # For using the `minted` package
-    texlab
-    # Markdown/Obsidian
-    (pkgs.rustPlatform.buildRustPackage {
-      pname = "markdown-oxide";
-      version = "0.0.3";
-      src = pkgs.fetchFromGitHub {
-        owner = "Feel-ix-343";
-        repo = "markdown-oxide";
-        rev = "fc710358651bc12f4f364295e2aa91215d458560";
-        sha256 = "sha256-vDSqWMVCQ/SVv/n8C7blLL7Anu94UDILuL0s80ouU+0=";
-      };
-      cargoLock = {
-        lockFile = builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/Feel-ix-343/markdown-oxide/main/Cargo.lock";
-          sha256 = "sha256:1z3g00vjcy5jha9n52f7sx9im6n42ay45wckvqil1x0h6ciw4vim";
+      # Fonts
+      iosevka
+      iosevka-term
+      sarasa-gothic
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+      # Shell
+      shellcheck
+      # C/C++
+      cmake
+      gnumake
+      gcc
+      clang-tools
+      # Python
+      python311
+      black
+      mypy
+      nodePackages.pyright
+      # JavaScript/TypeScript
+      nodejs
+      typescript
+      nodePackages.typescript-language-server
+      unstable.prettierd
+      # OCaml
+      ocamlformat
+      ocamlPackages.ocaml-lsp
+      dune_3
+      # Nix
+      unstable.nil
+      unstable.nixd
+      unstable.nixfmt-rfc-style
+      # Lua
+      sumneko-lua-language-server
+      stylua
+      selene
+      # LaTeX
+      python311Packages.pygments # For using the `minted` package
+      texlab
+      # Markdown/Obsidian
+      (pkgs.rustPlatform.buildRustPackage {
+        pname = "markdown-oxide";
+        version = "0.0.3";
+        src = pkgs.fetchFromGitHub {
+          owner = "Feel-ix-343";
+          repo = "markdown-oxide";
+          rev = "fc710358651bc12f4f364295e2aa91215d458560";
+          sha256 = "sha256-vDSqWMVCQ/SVv/n8C7blLL7Anu94UDILuL0s80ouU+0=";
         };
-        outputHashes = {
-          "tower-lsp-0.20.0" = "sha256-QRP1LpyI52KyvVfbBG95LMpmI8St1cgf781v3oyC3S4=";
+        cargoLock = {
+          lockFile = builtins.fetchurl {
+            url = "https://raw.githubusercontent.com/Feel-ix-343/markdown-oxide/main/Cargo.lock";
+            sha256 = "sha256:1z3g00vjcy5jha9n52f7sx9im6n42ay45wckvqil1x0h6ciw4vim";
+          };
+          outputHashes = {
+            "tower-lsp-0.20.0" = "sha256-QRP1LpyI52KyvVfbBG95LMpmI8St1cgf781v3oyC3S4=";
+          };
         };
-      };
-    })
-  ];
+      })
+    ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
