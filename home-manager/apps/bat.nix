d3977cadba984b0ff20a7436f80f7a267174fc15
Kyle Chui <kyle.chui+github@pm.me>
2024-06-06 22:11:15 -0700

    feat(bat): Use `modus_operandi` theme.
    TODO: If the PR gets merged, switch to the submodule syntax, since
    reading in the theme as an array of lines will be deprecated soon.

--- home-manager/apps/bat.nix
+++ home-manager/apps/bat.nix
@@ -3,17 +3,9 @@
 {
   programs.bat = {
     enable = true;
-    config.theme = "kanagawa";
+    config.theme = "modus_operandi";
     themes = {
-      kanagawa = {
-        src = pkgs.fetchFromGitHub {
-          owner = "kylechui";
-          repo = "kanagawa.nvim";
-          rev = "a8b360fa5fb93dc4733da01408ca1806bd227e86";
-          sha256 = "sha256-9peOxX4UqeqtZmNiUaNs/3uH8rsqIA7AoK1DCdVOeSw=";
-        };
-        file = "extras/kanagawa.tmTheme";
-      };
+      modus_operandi = builtins.readFile ./modus_operandi.tmTheme;
     };
   };
 }
