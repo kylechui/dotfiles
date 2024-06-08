14af80764b00bfc1087754361d08d3a201a6daf1
Kyle Chui <kyle.chui+github@pm.me>
2024-06-07 20:48:56 -0700

    refactor(bat): Fetch theme from upstream.

--- home-manager/apps/bat.nix
+++ home-manager/apps/bat.nix
@@ -5,7 +5,15 @@
     enable = true;
     config.theme = "modus_operandi";
     themes = {
-      modus_operandi = builtins.readFile ./modus_operandi.tmTheme;
+      modus_operandi = {
+        src = pkgs.fetchFromGitHub {
+          owner = "miikanissi";
+          repo = "modus-themes.nvim";
+          rev = "181dc38ddbcf9ad94d454cb06b13c859d4ff28d1";
+          sha256 = "sha256-yLcNfDUwS0neA9N6LCxTtLtVAjzdZfXIJ9OVnOM42WA=";
+        };
+        file = "extras/bat/modus_operandi.tmTheme";
+      };
     };
   };
 }
