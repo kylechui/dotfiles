4e1fb51b7d70b113d3326b39fac9bcf488ac3295
Kyle Chui <kyle.chui+github@pm.me>
2025-11-21 21:16:39 -0500

    feat(st): Update from v0.8.5 -> v0.9.3.

--- home-manager/apps/st/st.nix
+++ home-manager/apps/st/st.nix
@@ -3,34 +3,22 @@
 pkgs.st.overrideAttrs (oldAttrs: {
   buildInputs = oldAttrs.buildInputs ++ [ pkgs.harfbuzz ];
   src = fetchTarball {
-    url = "https://dl.suckless.org/st/st-0.8.5.tar.gz";
-    sha256 = "sha256:0iy7sj40m5x7wr4qkicijckk3cb0h9815mzacfjb3xrlrvpx6hm5";
+    url = "https://dl.suckless.org/st/st-0.9.3.tar.gz";
+    sha256 = "sha256:1c30c2dxw6y627dhas1gi68ivz9zcfqz9xy0rvz0jmlclfslkgay";
   };
   patches = oldAttrs.patches ++ [
     (pkgs.fetchpatch {
       url = "https://st.suckless.org/patches/xresources-with-reload-signal/st-xresources-signal-reloading-20220407-ef05519.diff";
       hash = "sha256-og6cJaMfn7zHfQ0xt6NKhuDNY5VK2CjzqJDJYsT5lrk=";
     })
-    (pkgs.fetchpatch {
-      url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.5.diff";
-      hash = "sha256-ZZAbrWyIaYRtw+nqvXKw8eXRWf0beGNJgoupRKsr2lc=";
-    })
-    (pkgs.fetchpatch {
-      url = "https://st.suckless.org/patches/scrollback/st-scrollback-reflow-0.8.5.diff";
-      hash = "sha256-GZq1DZl54NwC169tEOskqcIqMKjBCgCSHYXTKddxzFg=";
-    })
-    (pkgs.fetchpatch {
-      url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-20220127-2c5edf2.diff";
-      hash = "sha256-CuNJ5FdKmAtEjwbgKeBKPJTdEfJvIdmeSAphbz0u3Uk=";
-    })
-    (pkgs.fetchpatch {
-      url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-altscreen-20220127-2c5edf2.diff";
-      hash = "sha256-8oVLgbsYCfMhNEOGadb5DFajdDKPxwgf3P/4vOXfUFo=";
-    })
-    ./st-boxdraw.diff
-    ./st-wideglyph.diff
-    ./st-clickable-urls.diff
-    ./st-ligatures.diff
-    ./st-keymaps.diff
+    ./001-st-0.9.3-scrollback.patch
+    ./002-st-0.9.3-scrollback-reflow.patch
+    ./003-st-0.9.3-scrollback-mouse.patch
+    ./004-st-0.9.3-scrollback-altscreen.patch
+    ./005-st-0.9.3-boxdraw.patch
+    ./006-st-0.9.3-wideglyphs.patch
+    ./007-st-0.9.3-clickurl.patch
+    ./008-st-0.9.3-ligatures.patch
+    ./009-st-0.9.3-keymaps.patch
   ];
 })
