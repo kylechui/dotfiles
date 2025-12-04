89032dfbed09c2310807158da8bee0e8314fc268
Kyle Chui <kyle.chui+github@pm.me>
2025-12-03 21:02:37 -0500

    feat: Upgrade to NixOS 25.11.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "My NixOS Configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -173,7 +173,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
